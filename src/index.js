032650d8bd7b18afd5586145b8107f5a60b3b4a7
srglbrkchn <75715053+srglbrkchn@users.noreply.github.com>
2021-11-28 21:31:53 +0100

    Destructure animals array into cat and dog objects. Destructure cat and dog objects, give customized name to properties on each object.

--- src/index.js
+++ src/index.js
@@ -1,23 +1,26 @@
-// CHALLENGE: uncomment the code below and see the car stats rendered
-// import React from "react";
-// import ReactDOM from "react-dom";
+import animals from "./data";
 
-// ReactDOM.render(
-//   <table>
-//     <tr>
-//       <th>Brand</th>
-//       <th>Top Speed</th>
-//     </tr>
-//     <tr>
-//       <td>{tesla.model}</td>
-//       <td>{teslaTopSpeed}</td>
-//       <td>{teslaTopColour}</td>
-//     </tr>
-//     <tr>
-//       <td>{honda.model}</td>
-//       <td>{hondaTopSpeed}</td>
-//       <td>{hondaTopColour}</td>
-//     </tr>
-//   </table>,
-//   document.getElementById("root")
-// );
+console.log(animals);
+
+const [cat,
+    dog] = animals;
+
+const {
+    name: catName,
+    sound: catSound = "purr"
+} = cat;
+const {
+    name: dogName,
+    sound: dogSound = "bark"
+} = dog;
+
+console.log(catName + ": " + catSound);
+console.log(dogName + ": " + dogSound);
+
+// CHALLENGE: uncomment the code below and see the car stats rendered import
+// React from "react"; import ReactDOM from "react-dom"; ReactDOM.render(
+// <table>     <tr>       <th>Brand</th>       <th>Top Speed</th>     </tr>
+// <tr>       <td>{tesla.model}</td>       <td>{teslaTopSpeed}</td>
+// <td>{teslaTopColour}</td>     </tr>     <tr>       <td>{honda.model}</td>
+//   <td>{hondaTopSpeed}</td>       <td>{hondaTopColour}</td>     </tr>
+// </table>,   document.getElementById("root") );
