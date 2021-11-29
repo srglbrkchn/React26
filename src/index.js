610313ffbaf11f6eeb92b7188f73218d5a752dfa
srglbrkchn <75715053+srglbrkchn@users.noreply.github.com>
2021-11-29 11:50:23 +0100

    Fix table in index.html.

--- src/index.js
+++ src/index.js
@@ -9,34 +9,46 @@ import animals, {useAnimals} from "./data";
 // catFood + " " + catWater); console.log(dogName + ": " + dogFood + " " +
 // dogWater); 
 
-
 // CHALLENGE: uncomment the code below and see the car stats rendered
 
 import React from "react";
 import ReactDOM from "react-dom";
-import cars from "./practice"; 
+import cars from "./practice";
 
-const [honda, tesla] = cars;
+const [honda,
+    tesla] = cars;
 
-// Destructuring tesla object
-const {coloursByPopularity:[teslaTopColour], speedStats:{topSpeed: teslaTopSpeed}} = tesla;
-const {coloursByPopularity:[hondaTopColour], speedStats:{topSpeed: hondaTopSpeed}} = honda;
+// Destructuring tesla & honda object
+const {
+    coloursByPopularity: [teslaTopColour],
+    speedStats: {
+        topSpeed: teslaTopSpeed
+    }
+} = tesla;
+const {
+    coloursByPopularity: [hondaTopColour],
+    speedStats: {
+        topSpeed: hondaTopSpeed
+    }
+} = honda;
 
 ReactDOM.render(
     <table>
-    <tr>
-        <th>Brand</th>
-        <th>Top Speed</th>
-        <th>Top Color</th>
-    </tr>
-    <tr>
-        <td>{tesla.model}</td>
-        <td>{teslaTopSpeed}</td>
-        <td>{teslaTopColour}</td>
-    </tr>
-    <tr>
-        <td>{honda.model}</td>
-        <td>{hondaTopSpeed}</td>
-        <td>{hondaTopColour}</td>
-    </tr>
+    <tbody>
+        <tr>
+            <th>Brand</th>
+            <th>Top Speed</th>
+            <th>Top Color</th>
+        </tr>
+        <tr>
+            <td>{tesla.model}</td>
+            <td>{teslaTopSpeed}</td>
+            <td>{teslaTopColour}</td>
+        </tr>
+        <tr>
+            <td>{honda.model}</td>
+            <td>{hondaTopSpeed}</td>
+            <td>{hondaTopColour}</td>
+        </tr>
+    </tbody>
 </table>, document.getElementById("root"));
